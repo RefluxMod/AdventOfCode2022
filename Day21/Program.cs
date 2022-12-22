36b6e6d60a983522941dcd6926883592050910ca
Daniel Björk <daniel_bjork@live.se>
2022-12-22 16:37:34 +0100

    Day 21

--- Day21/Program.cs
+++ Day21/Program.cs
@@ -1,8 +1,8 @@
 ﻿var jobs = new Dictionary<string, (char Op, string Left, string Right, Func<long?> Result)>();
 var reverse = new Dictionary<string, Func<long?>>();
 
-foreach (var line in File.ReadAllLines("input.txt"))
-    jobs[line[..4]] = long.TryParse(line[6..], out long num) ? (' ', "", "", () => num) : GetJob(line[11], line[6..10], line[13..]); // Antingen så är jobbet ett konstant värde eller så är det ett uträknat värde
+foreach (var line in File.ReadAllLines("input.txt")) // Antingen har jobbet ett konstant värde eller så har det ett uträknat värde
+    jobs[line[..4]] = long.TryParse(line[6..], out long num) ? (' ', "", "", () => num) : GetJob(line[11], line[6..10], line[13..]);
 
 var root = jobs["root"];
 Console.WriteLine("Part1: " + root.Result()); // Genom att anropa root func så traverseras hela funktionskedjan
@@ -10,7 +10,7 @@ Console.WriteLine("Part1: " + root.Result()); // Genom att anropa root func så 
 foreach(var (k, j) in jobs.Where(x => x.Value.Op != ' ')) // Skapa omvända funktioner så att vi kan räkna baklänges
     (reverse[j.Left], reverse[j.Right]) = GetReverseFunc(k, j.Op, jobs[j.Left].Result(), jobs[j.Right].Result());
 
-jobs["humn"] = (' ', "", "", () => null); // Nollställ humn så att hela funktionskedjan kan räknas om. Den innehåller då endast värden som går att räkna fram utifrån konstanterna
+jobs["humn"] = (' ', "", "", () => null); // Nollställ humn så att hela funktionskedjan kan räknas om. Kedjan innehåller då endast värden som går att räkna fram utifrån konstanterna
 var value = jobs[root.Left].Result() ?? jobs[root.Right].Result(); // Vi vet att root vill ha två lika värden. Hämta det värdet som gick att räkna fram.
 reverse[root.Left] = () => value;
 reverse[root.Right] = () => value;
